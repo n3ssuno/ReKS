036b6bab4e490d0e4b20c7d8f7710cee50155013
Carlo Bottai <carlo.bottai@gmail.com>
2018-04-21 10:40:35 +0200

    The "coherence" function was splitted in two (similarly to the "entropy_decomposition" functions). The first computes the coherence index for cross-section data, while the second applies it to each year for a panel data set. Other changes have been needed to do that.

--- R/coherence.R
+++ R/coherence.R
@@ -40,106 +40,109 @@
 #' @param kng_nbr It is the name of the column of the data.frame that
 #' represents the numerosity of each knowledge class (e.g., the number of
 #' patents a region has in a given year in a given patent class).
-#' @param time_dim It is the name of the column of the data.frame that
-#' represents its temporal dimension (e.g., the different years of analysis).
-#' It is an optional parameter
-#' @return A data.frame with the Coherence Index of each geographical area
-#' in each time step of analysis.
+#' @param null_model It is useful to reuse this code in the coherence_panel
+#' function, leave if NULL unless you have an object of class reks_null_model
+#' that you whant to use as null model.
+#' @param names_as_strings If TRUE the function assumes you have written the
+#' name of the columns above said as strings, otherwise they are assumed to
+#' be symbols. Default is FALSE.
+#' @return A data.frame with the Coherence Index of each geographical area.
 #' @examples
-#' RCI <- coherence(data = df, time_dim = year, geo_dim = NUTS2,
+#' RCI <- coherence(data = df, geo_dim = NUTS2,
 #' kng_dim = IPC.3dig, kng_nbr = N.patents)
 
-coherence <- function(data, geo_dim, kng_dim, kng_nbr, time_dim = NULL) {
+coherence <- function(data, geo_dim, kng_dim, kng_nbr,
+                      null_model = NULL, names_as_strings = FALSE) {
+
+    # Preliminary operations, checks and transformations ---------------
+
     if (!requireNamespace("reshape2", quietly = TRUE)) {
         stop(paste0("Package \"reshape2\" needed for this function to work. ",
-            "Please install it."), call. = FALSE)
+                    "Please install it."), call. = FALSE)
     }
-    #library(reshape2)
     if (!requireNamespace("plyr", quietly = TRUE)) {
         stop(paste0("Package \"plyr\" needed for this function to work. ",
-            "Please install it."), call. = FALSE)
+                    "Please install it."), call. = FALSE)
     }
-    #library(plyr)
 
     data <- as.data.frame(data)
 
-    time_dim <- deparse(substitute(time_dim))
-    geo_dim <- deparse(substitute(geo_dim))
-    kng_dim <- deparse(substitute(kng_dim))
-    kng_nbr <- deparse(substitute(kng_nbr))
-
-    time_dim_added <- FALSE
-    if (time_dim == "NULL") {
-        data$AddedTimeDim <- 1
-        time_dim <- "AddedTimeDim"
-        time_dim_added <- TRUE
+    if (names_as_strings == FALSE) {
+        geo_dim <- deparse(substitute(geo_dim))
+        kng_dim <- deparse(substitute(kng_dim))
+        kng_nbr <- deparse(substitute(kng_nbr))
     }
 
-    data <- data[complete.cases(data[, c(time_dim, geo_dim,
-                                         kng_dim, kng_nbr)]), ]
+    data <- data[complete.cases(data[, c(geo_dim, kng_dim, kng_nbr)]), ]
 
-    C <- unique(data[, c(geo_dim, kng_dim)])
-    C$C <- 1
-    C <- reshape2::dcast(C,
-                         formula(paste(geo_dim, "~", kng_dim)),
-                         value.var = "C")
-    C[is.na(C)] <- 0
-    gnames <- C[, 1]
-    C <- as.matrix(C[, -1])
-    rownames(C) <- gnames
+    # Null model (Hypergeometric distribution) ---------------
 
-    J <- t(C) %*% C
+    if (is.null(null_model)) {
+        C <- unique(data[, c(geo_dim, kng_dim)])
+        C$C <- 1
+        C <- reshape2::dcast(C,
+                             formula(paste(geo_dim, "~", kng_dim)),
+                             value.var = "C")
+        C[is.na(C)] <- 0
+        gnames <- C[, 1]
+        C <- as.matrix(C[, -1])
+        rownames(C) <- gnames
 
-    n <- colSums(C)
+        J <- t(C) %*% C
 
-    mu <- n %*% t(n)
-    mu <- mu/nrow(C)
-    rownames(mu) <- colnames(mu)
+        n <- colSums(C)
 
-    s2 <- (1 - n/nrow(C)) %*% t((nrow(C) - n)/(nrow(C) - 1))
-    s2 <- mu * s2
-    rownames(s2) <- colnames(s2)
+        mu <- n %*% t(n)
+        mu <- mu/nrow(C)
+        rownames(mu) <- colnames(mu)
 
-    t <- (J - mu)/sqrt(s2)
+        s2 <- (1 - n/nrow(C)) %*% t((nrow(C) - n)/(nrow(C) - 1))
+        s2 <- mu * s2
+        rownames(s2) <- colnames(s2)
 
-    t1 <- t
-    diag(t1) <- 0
+        t <- (J - mu)/sqrt(s2)
 
-    ones <- matrix(1, nrow = nrow(t), ncol = nrow(t))
-    ones1 <- ones
-    diag(ones1) <- 0
+        t1 <- t
+        diag(t1) <- 0
 
-    RCI <- lapply(unique(data[, time_dim]), function(t) {
-        ee <- reshape2::dcast(data[which(data[, time_dim] == t), ],
-                              formula(paste(geo_dim, "~", kng_dim)),
-                              value.var = kng_nbr, fun.aggregate = sum)
-        gnames <- ee[, 1]
-        ee <- as.matrix(ee[, -1])
-        rownames(ee) <- gnames
-        en <- colnames(ee)
-        nc <- setdiff(colnames(t1), colnames(ee))
-        en <- c(en, nc)
-        for (i in nc) {
-            ee <- cbind(ee, 0)
-        }
-        colnames(ee) <- en
-        col.order <- colnames(t1)
-        ee <- ee[, col.order]
+        ones <- matrix(1, nrow = nrow(t), ncol = nrow(t))
+        ones1 <- ones
+        diag(ones1) <- 0
 
-        WAR <- ee %*% t1/ee %*% ones1
-        R <- rowSums(WAR * ee/rowSums(ee))
-        R[is.nan(R)] <- 0
-        R <- as.data.frame(R)
-        R <- cbind(rownames(R), R)
-        colnames(R) <- c(geo_dim, as.character(t))
-        return(R)
-    })
-    names(RCI) <- unique(data[, time_dim])
-    RCI <- plyr::join_all(RCI, geo_dim)
-    RCI <- reshape2::melt(RCI)
-    colnames(RCI) <- c(geo_dim, time_dim, "Coherence.Index")
-    if (time_dim_added) {
-        RCI <- RCI[, c(geo_dim, "Coherence.Index")]
+        null_model <- list()
+        null_model$t1 <- t1
+        null_model$ones1 <- ones1
+        class(null_model) <- "reks_null_model"
     }
-    return(RCI)
+
+    # Coherence index ---------------
+
+    if (class(null_model) != "reks_null_model") {
+        stop('null_model must be of "reks_null_model" class')
+    }
+
+    ee <- reshape2::dcast(data,
+                          formula(paste(geo_dim, "~", kng_dim)),
+                          value.var = kng_nbr, fun.aggregate = sum)
+    gnames <- ee[, 1]
+    ee <- as.matrix(ee[, -1])
+    rownames(ee) <- gnames
+    en <- colnames(ee)
+    nc <- setdiff(colnames(null_model$t1), colnames(ee))
+    en <- c(en, nc)
+    for (i in nc) {
+        ee <- cbind(ee, 0)
+    }
+    colnames(ee) <- en
+    col.order <- colnames(null_model$t1)
+    ee <- ee[, col.order]
+
+    WAR <- ee %*% null_model$t1 / ee %*% null_model$ones1
+    R <- rowSums(WAR * ee/rowSums(ee))
+    R[is.nan(R)] <- 0
+    R <- as.data.frame(R)
+    R <- cbind(rownames(R), R)
+    colnames(R) <- c(geo_dim, "Coherence.Index")
+
+    return(R)
 }
---
+++ R/coherence_panel.R
@@ -0,0 +1,127 @@
+#' @name
+#' coherence_panel
+#'
+#' @title
+#' Regional Coherence Index for panel data
+#'
+#' @description
+#' The function computes the so called Coherence index.
+#'
+#' @details
+#' The function computes the so called Coherence index.
+#' It assumes that the "universe" from which you derive the distribution of
+#' reference is composed by all the information provided in the database, and
+#' only this.
+#' You can use it both on a panel data set (if you identify also a column with
+#' a temporal indication of the observations) or on cross-section data (by
+#' leaveing the parameter just said as NULL).
+#' See:
+#' \itemize{
+#' \item{Teece, Rumelt, Dosi and Winter (1994) 'Understanding Corporate
+#' Coherence: Theory and Evidence', \emph{Journal of Economic Behavior \&
+#' Organization}, 23, 1-30;}
+#' \item{Nesta and Saviotti (2005) 'Coherence of the Knowledge Base and the
+#' Firm's Innovative Performance: Evidence from the U.S. Pharmaceutical
+#' Industry', \emph{Journal of Industrial Economics}, 53, 123-142;}
+#' \item{Nesta and Saviotti (2006) 'Firm Knowledge and Market Value in
+#' Biotechnology', \emph{Industrial and Corporate Change}, 15, 625-652;}
+#' \item{Quatraro (2010) 'Knowledge Coherence, Variety and Economic Growth:
+#' Manufacturing Evidence from Italian Regions', \emph{Research Policy}, 39,
+#' 1289-1302.}
+#'
+#' }
+#' @param data It is expected to be a dataframe in "long" format.
+#' @param geo_dim It is the name of the column of the data.frame that
+#' represents its geographical dimension (e.g., the different regions of
+#' analysis).
+#' @param kng_dim It is the name of the column of the data.frame that
+#' represents its knowledge dimension (e.g., the different patent classes of
+#' analysis).
+#' @param kng_nbr It is the name of the column of the data.frame that
+#' represents the numerosity of each knowledge class (e.g., the number of
+#' patents a region has in a given year in a given patent class).
+#' @param time_dim It is the name of the column of the data.frame that
+#' represents its temporal dimension (e.g., the different years of analysis).
+#' @return A data.frame with the Coherence Index of each geographical area
+#' in each time step of analysis.
+#' @examples
+#' RCI <- coherence(data = df, time_dim = year, geo_dim = NUTS2,
+#' kng_dim = IPC.3dig, kng_nbr = N.patents)
+
+coherence_panel <- function(data, geo_dim, kng_dim, kng_nbr, time_dim) {
+
+    # Preliminary operations, checks and transformations ---------------
+
+    if (!requireNamespace("reshape2", quietly = TRUE)) {
+        stop(paste0("Package \"reshape2\" needed for this function to work. ",
+                    "Please install it."), call. = FALSE)
+    }
+    if (!requireNamespace("plyr", quietly = TRUE)) {
+        stop(paste0("Package \"plyr\" needed for this function to work. ",
+                    "Please install it."), call. = FALSE)
+    }
+
+    data <- as.data.frame(data)
+
+    time_dim <- deparse(substitute(time_dim))
+    geo_dim <- deparse(substitute(geo_dim))
+    kng_dim <- deparse(substitute(kng_dim))
+    kng_nbr <- deparse(substitute(kng_nbr))
+
+    data <- data[complete.cases(data[, c(time_dim, geo_dim,
+                                         kng_dim, kng_nbr)]), ]
+
+    # Null model (Hypergeometric distribution) ---------------
+
+    C <- unique(data[, c(geo_dim, kng_dim)])
+    C$C <- 1
+    C <- reshape2::dcast(C,
+                         formula(paste(geo_dim, "~", kng_dim)),
+                         value.var = "C")
+    C[is.na(C)] <- 0
+    gnames <- C[, 1]
+    C <- as.matrix(C[, -1])
+    rownames(C) <- gnames
+
+    J <- t(C) %*% C
+
+    n <- colSums(C)
+
+    mu <- n %*% t(n)
+    mu <- mu/nrow(C)
+    rownames(mu) <- colnames(mu)
+
+    s2 <- (1 - n/nrow(C)) %*% t((nrow(C) - n)/(nrow(C) - 1))
+    s2 <- mu * s2
+    rownames(s2) <- colnames(s2)
+
+    t <- (J - mu)/sqrt(s2)
+
+    t1 <- t
+    diag(t1) <- 0
+
+    ones <- matrix(1, nrow = nrow(t), ncol = nrow(t))
+    ones1 <- ones
+    diag(ones1) <- 0
+
+    null_model <- list()
+    null_model$t1 <- t1
+    null_model$ones1 <- ones1
+    class(null_model) <- "reks_null_model"
+
+    # Coherence index for panel data ---------------
+
+    RCI <- lapply(unique(data[, time_dim]), function(t) {
+        data_subset <- data[which(data[, time_dim] == t), ]
+        R <- coherence(data_subset, geo_dim, kng_dim, kng_nbr,
+                       null_model = null_model, names_as_strings = TRUE)
+        colnames(R) <- c(geo_dim, as.character(t))
+        return(R)
+    })
+
+    RCI <- plyr::join_all(RCI, geo_dim)
+    RCI <- reshape2::melt(RCI)
+    colnames(RCI) <- c(geo_dim, time_dim, "Coherence.Index")
+
+    return(RCI)
+}
